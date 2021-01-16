30291233fde30e179ee061effe469004c0ce43c5
Thiago Machado <ti-thiago@outlook.com>
2021-01-15 22:53:50 -0300

    Finalização do segundo módulo

--- js/principal.js
+++ js/principal.js
@@ -13,3 +13,33 @@ titulo.textContent = "Aparecida Nutricionista";
 // pegando a tag e seu conteudo usando a classe
 var tituloPorClasse = document.querySelector(".titulo");
 console.log("Título usando a classe: ", tituloPorClasse.textContent);
+
+// calculando IMC
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+var tdImc = paciente.querySelector(".info-imc");
+
+// condições lógicas
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if (altura <= 0 || altura >= 3.00){
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!";
+}
+
+if (alturaEhValida && pesoEhValido){
+    tdImc.textContent = peso / (altura * altura);
+}
